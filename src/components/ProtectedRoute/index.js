cd350887cfe792e66152da0530698cab10809f88
patnaik <prataparaopatnaik@gmail.com>
2024-10-04 07:28:40 +0000

    EnhancementOfRestaurantApp

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import {Redirect, Route} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (!jwtToken) {
+    return <Redirect to="/login" />
+  }
+
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
